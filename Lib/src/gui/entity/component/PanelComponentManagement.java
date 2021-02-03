23e93dcfc74e681038d49ce6df1fd564ed1a39be
CorvusOps <josepholivar124@gmail.com>
2021-02-03 14:38:13 +0800

    initial commit on planned files
    GUI files working in progress

---
+++ Lib/src/gui/entity/component/PanelComponentManagement.java
@@ -0,0 +1,14 @@
+package gui.entity.component;
+
+import javax.swing.JPanel;
+
+public class PanelComponentManagement extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelComponentManagement() {
+
+	}
+
+}
---
+++ Lib/src/gui/entity/subject/PanelSubjectManagement.java
@@ -0,0 +1,14 @@
+package gui.entity.subject;
+
+import javax.swing.JPanel;
+
+public class PanelSubjectManagement extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelSubjectManagement() {
+
+	}
+
+}
