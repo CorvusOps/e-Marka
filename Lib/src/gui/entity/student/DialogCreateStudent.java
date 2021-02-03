847c226ae88b12e1504cacd191dd14da543ec7f5
CorvusOps <josepholivar124@gmail.com>
2021-02-03 20:15:24 +0800

    Update student GUI

--- Lib/src/gui/entity/student/DialogCreateStudent.java
+++ Lib/src/gui/entity/student/DialogCreateStudent.java
@@ -1,55 +1,298 @@
 package gui.entity.student;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import domain.Student;
+import domain.Subject;
+
+@SuppressWarnings("serial")
 public class DialogCreateStudent extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
-
+private final JPanel contentPanel = new JPanel();
+	
 	/**
-	 * Launch the application.
+	 * The link to the student management frame that this Dialog is owned by.
+	 * This is used to refer to the student repository object in there.
 	 */
-	public static void main(String[] args) {
-		try {
-			DialogCreateStudent dialog = new DialogCreateStudent();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	protected PanelStudentManagement studentManagementFrame;
+	
+	// Input Fields
+	private JTextField jtxtfldStudentNumber;
+	private JTextField jtxtfldFirstName;
+	private JTextField jtxtfldMiddleName;
+	private JTextField jtxtfldLastName;
+	private JTextField jtxtfldAddress;
+	private JTextField jtxtfldSection;
+	private JComboBox<Subject> jcmbSubject;
 
 	/**
 	 * Create the dialog.
 	 */
 	public DialogCreateStudent() {
+		/* This Dialog's Properties */
+		setPreferredSize(new Dimension(500, 300));
+		setMinimumSize(new Dimension(500, 300));
+		setTitle("Add Student");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPanel.setBorder(new EmptyBorder(10, 10, 0, 10));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
-	}
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		/* END OF dialog properties */
+		
+		/* jlblStudentNumber - student no. label */
+		JLabel jlblStudentNumber = new JLabel("Student Number:");
+		jlblStudentNumber.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblStudentNumber = new GridBagConstraints();
+		gbc_jlblStudentNumber.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblStudentNumber.anchor = GridBagConstraints.EAST;
+		gbc_jlblStudentNumber.gridx = 0;
+		gbc_jlblStudentNumber.gridy = 0;
+		contentPanel.add(jlblStudentNumber, gbc_jlblStudentNumber);
+		/* END OF jlblStudentNumber */
 
+		/* jtxtfldStudentNumber - student no. input */
+		jtxtfldStudentNumber = new JTextField();
+		jtxtfldStudentNumber.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jtxtfldStudentNumber = new GridBagConstraints();
+		gbc_jtxtfldStudentNumber.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtfldStudentNumber.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtfldStudentNumber.gridx = 1;
+		gbc_jtxtfldStudentNumber.gridy = 0;
+		contentPanel.add(jtxtfldStudentNumber, gbc_jtxtfldStudentNumber);
+		jtxtfldStudentNumber.setColumns(10);
+		/* END OF jtxtfldStudentNumber */
+	
+		/* jlblFirstName - first name label */
+		JLabel jlblFirstName = new JLabel("First Name:");
+		jlblFirstName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblFirstName = new GridBagConstraints();
+		gbc_jlblFirstName.anchor = GridBagConstraints.EAST;
+		gbc_jlblFirstName.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblFirstName.gridx = 0;
+		gbc_jlblFirstName.gridy = 1;
+		contentPanel.add(jlblFirstName, gbc_jlblFirstName);
+		/* END OF jlblFirstName */
+
+		/* jtxtfldFirstName - first name input */
+		jtxtfldFirstName = new JTextField();
+		jtxtfldFirstName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jtxtfldFirstName.setColumns(10);
+		GridBagConstraints gbc_jtxtfldFirstName = new GridBagConstraints();
+		gbc_jtxtfldFirstName.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtfldFirstName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtfldFirstName.gridx = 1;
+		gbc_jtxtfldFirstName.gridy = 1;
+		contentPanel.add(jtxtfldFirstName, gbc_jtxtfldFirstName);
+		/* END OF jtxtfldFirstName */
+
+		/* jlblMiddleName - middle name label */
+		JLabel jlblMiddleName = new JLabel("Middle Name:");
+		jlblMiddleName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblMiddleName = new GridBagConstraints();
+		gbc_jlblMiddleName.anchor = GridBagConstraints.EAST;
+		gbc_jlblMiddleName.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblMiddleName.gridx = 0;
+		gbc_jlblMiddleName.gridy = 2;
+		contentPanel.add(jlblMiddleName, gbc_jlblMiddleName);
+		/* END OF jlblMiddleName */
+
+		/* jtxtfldMiddleName - middle name input */
+		jtxtfldMiddleName = new JTextField();
+		jtxtfldMiddleName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jtxtfldMiddleName.setColumns(10);
+		GridBagConstraints gbc_jtxtMiddleName = new GridBagConstraints();
+		gbc_jtxtMiddleName.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtMiddleName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtMiddleName.gridx = 1;
+		gbc_jtxtMiddleName.gridy = 2;
+		contentPanel.add(jtxtfldMiddleName, gbc_jtxtMiddleName);
+		/* END OF jtxtfldMiddleName */
+	
+		/* jlblLastName - last name label */
+		JLabel jlblLastName = new JLabel("Last Name:");
+		jlblLastName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblLastName = new GridBagConstraints();
+		gbc_jlblLastName.anchor = GridBagConstraints.EAST;
+		gbc_jlblLastName.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblLastName.gridx = 0;
+		gbc_jlblLastName.gridy = 3;
+		contentPanel.add(jlblLastName, gbc_jlblLastName);
+		/* END OF jlblLastName */
+	
+		/* jtxtfldLastName - last name input */
+		jtxtfldLastName = new JTextField();
+		jtxtfldLastName.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jtxtfldLastName.setColumns(10);
+		GridBagConstraints gbc_jtxtfldLastName = new GridBagConstraints();
+		gbc_jtxtfldLastName.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtfldLastName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtfldLastName.gridx = 1;
+		gbc_jtxtfldLastName.gridy = 3;
+		contentPanel.add(jtxtfldLastName, gbc_jtxtfldLastName);
+		/* END OF jtxtfldLastName */
+
+		/* jlblAddress - address label */
+		JLabel jlblAddress = new JLabel("Address");
+		jlblAddress.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblAddress = new GridBagConstraints();
+		gbc_jlblAddress.anchor = GridBagConstraints.EAST;
+		gbc_jlblAddress.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblAddress.gridx = 0;
+		gbc_jlblAddress.gridy = 4;
+		contentPanel.add(jlblAddress, gbc_jlblAddress);
+		/* END OF jlblAddress */
+	
+		/* jtxtfldAddress - address input */
+		jtxtfldAddress = new JTextField();
+		jtxtfldAddress.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jtxtfldAddress.setColumns(10);
+		GridBagConstraints gbc_jtxtfldAddress = new GridBagConstraints();
+		gbc_jtxtfldAddress.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtfldAddress.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtfldAddress.gridx = 1;
+		gbc_jtxtfldAddress.gridy = 4;
+		contentPanel.add(jtxtfldAddress, gbc_jtxtfldAddress);
+		/* END OF jtxtfldAddress */
+	
+		/* jlblSection - section label */
+		JLabel jlblSection = new JLabel("Section:");
+		jlblSection.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblSection = new GridBagConstraints();
+		gbc_jlblSection.anchor = GridBagConstraints.EAST;
+		gbc_jlblSection.insets = new Insets(0, 0, 5, 5);
+		gbc_jlblSection.gridx = 0;
+		gbc_jlblSection.gridy = 5;
+		contentPanel.add(jlblSection, gbc_jlblSection);
+		/* END OF jlblSection */
+
+		/* jtxtfldSection - section input */
+		jtxtfldSection = new JTextField();
+		jtxtfldSection.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jtxtfldSection.setColumns(10);
+		GridBagConstraints gbc_jtxtfldSection = new GridBagConstraints();
+		gbc_jtxtfldSection.insets = new Insets(0, 0, 5, 0);
+		gbc_jtxtfldSection.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtxtfldSection.gridx = 1;
+		gbc_jtxtfldSection.gridy = 5;
+		contentPanel.add(jtxtfldSection, gbc_jtxtfldSection);
+		
+		JLabel jlblSubject = new JLabel("Subject:");
+		jlblSubject.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jlblSubject = new GridBagConstraints();
+		gbc_jlblSubject.anchor = GridBagConstraints.EAST;
+		gbc_jlblSubject.insets = new Insets(0, 0, 0, 5);
+		gbc_jlblSubject.gridx = 0;
+		gbc_jlblSubject.gridy = 6;
+		contentPanel.add(jlblSubject, gbc_jlblSubject);
+		
+		jcmbSubject = new JComboBox<>();
+		jcmbSubject.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_jcmbSubject = new GridBagConstraints();
+		gbc_jcmbSubject.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jcmbSubject.gridx = 1;
+		gbc_jcmbSubject.gridy = 6;
+		contentPanel.add(jcmbSubject, gbc_jcmbSubject);
+		/* END OF jtxtfldSection */
+	
+		/* jpnlButtons - button actions panel */
+		JPanel jpnlButtons = new JPanel();
+		jpnlButtons.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(jpnlButtons, BorderLayout.SOUTH);
+		/* END OF jpnlButtons */
+
+		/* jbtnOk - save student button */
+		JButton jbtnOk = new JButton("OK");
+		jbtnOk.setActionCommand("OK");
+		jpnlButtons.add(jbtnOk);
+		// When this button is clicked, execute actionPerformed
+		jbtnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Retrieve inputs, then create a Student object out of it
+				Student student =
+						new Student(
+								jtxtfldStudentNumber.getText(),
+								jtxtfldFirstName.getText(),
+								jtxtfldMiddleName.getText(),
+								jtxtfldLastName.getText(),
+								jtxtfldAddress.getText(),
+								jtxtfldSection.getText(),
+								(Subject) jcmbSubject.getSelectedItem());
+
+				// Clear fields
+				clearFields();
+				// Save the parsed Student
+				studentManagementFrame.studentRepository.save(student);
+				
+				// Output friendly message
+				JOptionPane.showMessageDialog(null, "Successfully saved student");
+				
+				// Refresh the TableModel, to prompt redraw of JTable
+				studentManagementFrame.studentTableModel.refresh();
+			}
+		});
+		getRootPane().setDefaultButton(jbtnOk);
+		/* END OF jbtnOk */
+	
+
+		/* jbtnCancel - hides this dialog box */
+		JButton jbtnCancel = new JButton("Cancel");
+		jbtnCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearFields();
+				setVisible(false);
+			}
+		});
+		jpnlButtons.add(jbtnCancel);
+		/* END OF jbtnCancel */
+		
+	}
+	
+	/**
+	 * Clears the TextFields in this Dialog.
+	 */
+	private void clearFields() {
+		jtxtfldStudentNumber.setText("");
+		jtxtfldFirstName.setText("");
+		jtxtfldMiddleName.setText("");
+		jtxtfldLastName.setText("");
+		jtxtfldAddress.setText("");
+		jtxtfldSection.setText("");
+	}
+	
+	public void refreshSubjectComboBox() {
+		List<Subject> subjectList = studentManagementFrame.subjectRepository.getAll();
+		
+		Subject[] subjectArray = new Subject[subjectList.size()];
+		for(int i = 0; i < subjectList.size(); i++)
+			subjectArray[i] = subjectList.get(i);
+		
+		jcmbSubject.setModel(new DefaultComboBoxModel<Subject>(subjectArray));
+	}
 }
--- Lib/src/gui/entity/student/TemplateStudent.java
+++ Lib/src/gui/entity/student/TemplateStudent.java
@@ -1,42 +1,122 @@
 package gui.entity.student;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.table.AbstractTableModel;
 
-public class TemplateStudent extends JFrame {
+import domain.Student;
 
-	private JPanel contentPane;
-
+@SuppressWarnings("serial")
+public class TemplateStudent extends AbstractTableModel {
+	
 	/**
-	 * Launch the application.
+	 * The link to the student management frame that this TableModel is owned by.
+	 * This is used to refer to the student repository object in there.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TemplateStudent frame = new TemplateStudent();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	protected PanelStudentManagement studentManagementFrame;
+	
+	/**
+	 * Cache of this TableModel. Refreshed only when refresh() is called.
+	 */
+	private static List<Student> currentValue;
+	
+	/**
+	 * Fixed column count of this TableModel, set to 4.
+	 */
+	@Override
+	public int getColumnCount() {
+		return 4;
+	}
+	
+	/**
+	 * Fetches the column names of this TableModel.
+	 */
+	@Override
+	public String getColumnName(int columnIndex) {
+		switch(columnIndex) {
+		
+		// First Column Header
+		case 0:
+			return "Student Number";
+			
+		// Second Column Header
+		case 1:
+			return "Name";
+				
+		// Third Column Header
+		case 2:
+			return "Address";
+			
+		// Fourth Column Header
+		case 3:
+			return "Section";
+			
+		default:
+			return null;
+		
+		}
+	}
+	
+	/**
+	 * Depending on the current value of the internal cache,
+	 * this returns its size.
+	 */
+	@Override
+	public int getRowCount() {
+		if(currentValue == null)
+			return 0;
+		
+		return currentValue.size();
+	}
+	
+	/**
+	 * Gets the value at the specified row and column.
+	 * Apparently, each row of the JTable should point to a Student object in currentValue,
+	 * and the column value is each field of Student.
+	 */
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if(currentValue == null)
+			return null;
+		
+		// Get the student object at the specified row,
+		// which coincidentally, is the same index in the currentValue List.
+		Student student = currentValue.get(rowIndex);
+		
+		// Depending on what columnIndex is given,
+		// return the proper field of the student
+		switch(columnIndex) {
+		
+		// First Column - student number
+		case 0:
+			return student.getStudentNumber();
+			
+		// Second Column - name
+		case 1:
+			return student.getFirstName() + " " + student.getLastName();
+			
+		// Third Column - address
+		case 2:
+			return student.getAddress();
+			
+		// Fourth Column - section
+		case 3:
+			return student.getSection();
+		
+		default:
+			return null;
+		
+		}
+	}
+	
+	/**
+	 * Refreshes this TableModel with new data. Also prompts the JTable to redraw everything
+	 * with fireTableDataChanged().
+	 */
+	public void refresh() {
+		currentValue = studentManagementFrame.studentRepository.getAll();
+		fireTableDataChanged();
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public TemplateStudent() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
 
 }
