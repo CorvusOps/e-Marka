a1d5230c6b94efa097b52b8bd06b3b2415ee72b8
CorvusOps <josepholivar124@gmail.com>
2021-02-18 17:03:56 +0800

    Enhance Grade and CRUDGrade
    Fix the compatibility and the words

--- Lib/src/repository/CRUDGrade.java
+++ Lib/src/repository/CRUDGrade.java
@@ -6,12 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import domain.Grade;
+import domain.GradePT;
+import domain.GradeQA;
+import domain.GradeWW;
 import domain.Subject;
 
 /**
@@ -46,8 +48,8 @@ public class CRUDGrade {
 	            		+ " WHERE subject_id = '" + subject.getId() + "'")) {
 	            
 	            while(resultSet.next()) {
-	                int studentNumber = resultSet.getInt(1);
-	                String studentFirstName = resultSet.getString(2),
+	            	String studentNumber = resultSet.getString(1),
+	            		   studentFirstName = resultSet.getString(2),
 	                       studentLastName = resultSet.getString(3);
 	                
 	                Grade grade = new Grade();
@@ -66,9 +68,81 @@ public class CRUDGrade {
 	        return gradeList;
 	    }
 	  
-	  
+	  // readByTable 
 	  // getStudentBySubjectID <- retrieve that specific student only from the list
 	  // updateGrades <- update grades from 0
 	  
+	  public Grade getByStudentNumberID(String studentNumber) {
+		  	Grade grade = new Grade();  
+	        
+	        try(Connection connection = dataSource.getConnection();
+	            Statement selectStatement = connection.createStatement();
+	            ResultSet resultSet = selectStatement.executeQuery("SELECT * FROM student" +
+	            									"WHERE student_number = '" + studentNumber + "'")) {
+	        	// what to get here again ???
+	        	if(resultSet.next()) {
+	                String studentFirstName = resultSet.getString(1),
+	                       studentLastName = resultSet.getString(2);
+	                
+	                grade.setStudentNumber(studentNumber);
+	                grade.setStudentName(studentFirstName + " " + studentLastName);
+	                
+	                grade.setGradeWW(gradeWWDAO.getAllByStudentNumber(studentNumber));
+	                grade.setGradePT(gradePTDAO.getAllByStudentNumber(studentNumber));
+	                grade.setGradeQA(gradeQADAO.getAllByStudentNumber(studentNumber));
+	            }
+	            
+	        } catch(SQLException e) {
+	            e.printStackTrace();
+	        }
+	        
+	        return grade;
+	    }
+	  
+	  public void update(Grade grade) {
+			try(
+				// Create a connection to the database, auto-closed later since we use try-with-resources
+				Connection connection = dataSource.getConnection();
+				// Prepare a placeholder object for an INSERT SQL Statement
+				PreparedStatement updateWWStatement =
+						connection.prepareStatement("UPDATE gradeww" + 
+								"SET gradeWW = ? " + 
+								"WHERE gradeWW_id = ? ");
+				PreparedStatement updatePTStatement =
+						connection.prepareStatement("UPDATE gradept" + 
+								"SET gradePT = ? " + 
+								"WHERE gradePT_id = ? ");
+				PreparedStatement updateQAStatement =
+						connection.prepareStatement("UPDATE gradeqa" + 
+								"SET gradeQA = ? " + 
+								"WHERE gradeQA_id = ? ")) {
+				
+				// Bind each field of the student object into the insert statement object.
+				
+				for(GradeWW gradeWW : grade.getGradeWW()) {
+					updateWWStatement.setInt(1, gradeWW.getGradesWW());
+					updateWWStatement.setInt(2, gradeWW.getGradeWW_id());
+					updateWWStatement.addBatch();
+				}
+				updateWWStatement.executeBatch();
+				
+				for(GradePT gradePT : grade.getGradePT()) {
+					updatePTStatement.setInt(1, gradePT.getGradesPT());
+					updatePTStatement.setInt(2, gradePT.getGradePT_id());
+					updatePTStatement.addBatch();
+				}
+				updatePTStatement.executeBatch();
+				
+				for(GradeQA gradeQA : grade.getGradeQA()) {
+					updateQAStatement.setInt(1, gradeQA.getGradesQA());
+					updateQAStatement.setInt(2, gradeQA.getGradeQA_id());
+					updateQAStatement.addBatch();
+				}
+				updateQAStatement.executeBatch();
+			
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	
 }
